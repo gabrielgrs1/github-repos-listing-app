0cf57a71707339598571a6a0655f98c5c049db72
Gabriel Guilherme <gabriel_guilherme2006@hotmail.com>
2022-12-12 23:17:31 -0300

    Refactor test home view model to page source test

--- app/src/test/java/com/gabrielgrs2/listrepos/data/SearchPagingSourceTest.kt
+++ app/src/test/java/com/gabrielgrs2/listrepos/data/SearchPagingSourceTest.kt
@@ -1,13 +1,11 @@
-package com.gabrielgrs2.listrepos.screens.home
+package com.gabrielgrs2.listrepos.data
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.paging.PagingSource
 import com.gabrielgrs2.listrepos.base.BaseUTTest
-import com.gabrielgrs2.listrepos.di.configureTestAppComponent
 import com.gabrielgrs2.listrepos.domain.model.Owner
 import com.gabrielgrs2.listrepos.domain.model.Repository
 import com.gabrielgrs2.listrepos.domain.usecase.home.GetSearchRepositoriesUseCase
-import com.gabrielgrs2.listrepos.presentation.home.HomeViewModel
 import com.gabrielgrs2.listrepos.screens.utils.FakeSearchPagingSource
 import io.mockk.MockKAnnotations
 import io.mockk.mockk
@@ -18,23 +16,45 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.koin.core.context.startKoin
 import kotlin.test.assertEquals
 
 @RunWith(JUnit4::class)
 @OptIn(ExperimentalCoroutinesApi::class)
-class HomeViewModelTest : BaseUTTest() {
+class SearchPagingSourceTest : BaseUTTest() {
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
-
-    var searchUseCase: GetSearchRepositoriesUseCase = mockk(relaxed = true)
-    var homeViewModel = HomeViewModel(searchUseCase)
+    val pagingSource = FakeSearchPagingSource()
 
     @Before
     fun start() {
         super.setUp()
         MockKAnnotations.init(this)
-        startKoin { modules(configureTestAppComponent(getMockWebServerUrl())) }
     }
+
+    @Test
+    fun `verify paging data is success when paging correctly loaded`() {
+        runTest {
+            val params: PagingSource.LoadParams.Refresh<Int> = PagingSource.LoadParams.Refresh(
+                key = null,
+                loadSize = 2,
+                placeholdersEnabled = false
+            )
+
+            val repositories = listOf(
+                Repository(1, Owner("", "teste"), "nome", 1, 1),
+                Repository(2, Owner("", "teste2"), "nome2", 2, 2)
+            )
+
+            assertEquals(
+                expected = PagingSource.LoadResult.Page(
+                    data = repositories,
+                    prevKey = null,
+                    nextKey = repositories[1].id
+                ), actual = pagingSource.load(params)
+            )
+
+        }
+    }
+
 }
